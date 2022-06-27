bcf289186c17035e5f3ea8301fdbc4c42c2abfe3
Emilio Campos <daval7@gmail.com>
2022-06-27 15:33:25 +0200

    Heroku ClearDB

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-    connectionUri: process.env.CLEARDB_DATABASE_URL || `mysql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}?reconnect=true`,
+    connectionUri: process.env.CLEARDB_DATABASE_URL || `mysql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}`,
     host: process.env.HOST || "",
     database: process.env.DATABASE || "",
     user: process.env.USER || "",
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
 
 const main = () => {
-    app.listen(app.get("port"));
-    console.log(`Server on port ${app.get("port")}`);
+    app.listen(app.get("port"), () => {
+        console.log('Server listening on port', app.get("port"));
+    });    
 };
 
 main();
